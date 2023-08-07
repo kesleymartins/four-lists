8dcde38dfc33ab216cc0319b822e66e3de531668
kesleymartins <kesley.martins@vivaldi.net>
2023-08-07 01:59:43 -0300

    config lists :index, :new, :create endpoints and set root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "lists#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :lists, only: [:index, :new, :create]
 end
