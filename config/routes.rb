6bcdcd73e78cd53dfc1e1d1d443dbe68b529aa35
kesleymartins <kesley.martins@vivaldi.net>
2023-09-10 21:48:14 -0300

    add list destroy endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'lists#index'
 
-  resources :lists, only: %i[index new edit create update]
+  resources :lists, only: %i[index new edit create update destroy]
 end
